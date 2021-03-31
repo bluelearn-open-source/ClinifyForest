42f253f171b285101059a2349b38edfd0373a6c4
Tilak <davetilak003@gmail.com>
2021-03-31 20:25:00 +0530

    debug true

--- ClinifyForest/settings.py
+++ ClinifyForest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'clinifyforest.herokuapp.com']
 
