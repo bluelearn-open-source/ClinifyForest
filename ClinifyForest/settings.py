eff1b0e1c7ae97a75d3ec40a00d197777a86294f
Tilak <davetilak003@gmail.com>
2021-04-01 11:39:44 +0530

    checking settings

--- ClinifyForest/settings.py
+++ ClinifyForest/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
 ]
 try:
     from .local_settings import *
