15f26fb73e6106804667f97083e743421f6b5774
Tilak <davetilak003@gmail.com>
2021-04-04 00:01:59 +0530

    static files

--- ClinifyForest/settings.py
+++ ClinifyForest/settings.py
@@ -40,7 +40,7 @@ AUTHENTICATION_BACKENDS = [
 
 
 INSTALLED_APPS = [
-    # 'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
