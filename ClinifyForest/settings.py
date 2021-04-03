3e3ee27f9839bd424d1bcedcb657c3afb94c415b
Tilak <davetilak003@gmail.com>
2021-04-03 22:52:35 +0530

    set timezone

--- ClinifyForest/settings.py
+++ ClinifyForest/settings.py
@@ -126,13 +126,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+5:30'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
