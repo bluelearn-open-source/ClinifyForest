108889d2497ad42479586a55330ef928f727ea37
Tilak <davetilak003@gmail.com>
2021-04-03 18:35:22 +0530

    static url

--- ClinifyForest/urls.py
+++ ClinifyForest/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from django.conf.urls.static import static
 from main.views import home, lb, store, reset
 from login.views import discord_logout, discord_login, discord_login_redirect
 from search.views import search
