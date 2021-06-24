d243f8348af6db835dd7081cf25a7ef331defa86
Tilak dave <davetilak003@gmail.com>
2021-06-24 21:57:07 +0530

    added new domain as allowed hosts

--- ClinifyForest/settings.py
+++ ClinifyForest/settings.py
@@ -28,7 +28,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['clinifyforest.herokuapp.com']
+ALLOWED_HOSTS = ['clinifyforest.herokuapp.com', 'forest.bluelearn.in']
 
 
 
