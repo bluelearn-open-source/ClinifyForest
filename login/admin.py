eba624ee3fd14c049a386ef31e0389fa825cd2d9
Tilak dave <davetilak003@gmail.com>
2021-08-30 11:51:45 +0530

    admin panel made better

--- login/admin.py
+++ login/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import DiscordUser
+from django.contrib.auth.models import Group
+
 # Register your models here.
-admin.site.register(DiscordUser)
+admin.site.unregister(Group)
+
+
+@admin.register(DiscordUser)
+class DiscordUserAdmin:
+    list_display = ("discord_tag", "coins", "trees",
+                    "deadtrees", "in_session", "slowmode")
+    list_filter = ("discord_tag", "coins", "trees",
+                   "deadtrees", "in_session", "slowmode")
+    search_fields = ("discord_tag",)
